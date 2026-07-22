80f75ab1fdcac62ace87f5080199bde3472f4535
AFelipeCR <andres@rosebushcrown.com>
2026-07-22 16:38:01 -0700

    Main bot configg

--- src/extensions.list.ts
+++ src/extensions.list.ts
@@ -1,4 +1,8 @@
+import { InfoExtension } from '@ivy-eco/info';
 import { ExtensionDefinition } from './modules/extension/extension.interface';
+import { AternosExtension } from '@ivy-eco/aternos';
 
 export const extensionsList: ExtensionDefinition[] = [
+    { module: InfoExtension },
+    { module: AternosExtension }
 ];
